627cc63cd1639cca332a68243c0ea2af1006400a
root <root@puppet-master>
2014-09-29 08:24:19 +0000

    add parameters support in vagrant_base module   - speficy $user and $group when use vagrant_base module

--- vagrant_base/manifests/init.pp
+++ vagrant_base/manifests/init.pp
@@ -1,55 +1,67 @@
 # /etc/puppet/modules/vagrant_base/manifests/init.pp
 
-class vagrant_base {
+class vagrant_base(
+        $user = 'vagrant', 
+        $group = 'vagrant', 
+    ) {
+
+    user { "${user}":
+        ensure     => "present",
+        managehome => true,
+        shell  => "/bin/bash",
+    }
+
 	package { ["vim", "screen"]:
 		ensure => installed,
 	}
 
-    file { "/home/vagrant/.screenrc":
+    file { "/home/${user}/.screenrc":
         ensure => present,
-        owner => "vagrant",
-        group => "vagrant",
+        owner => $user,
+        group => $group,
         alias => "screen-config",
         source => "puppet:///modules/vagrant_base/screenrc",
-        require => Package["screen"],
+        require => [Package["screen"], User["${user}"]],
     }
 
-    file { "/home/vagrant/.vimrc":
+    file { "/home/${user}/.vimrc":
         ensure => present,
-        owner => "vagrant",
-        group => "vagrant",
+        owner => $user,
+        group => $group,
         alias => "vim-config",
         source => "puppet:///modules/vagrant_base/vimrc",
-        require => Package["vim"],
+        require => [Package["vim"], User["${user}"]],
     }
 
-    file { "/home/vagrant/.bashrc":
+    file { "/home/${user}/.bashrc":
         ensure => present,
-        owner => "vagrant",
-        group => "vagrant",
+        owner => $user,
+        group => $group,
         alias => "bashrc",
         source => "puppet:///modules/vagrant_base/bashrc",
+        require => User["${user}"],
     }
 
-    file { "/home/vagrant/.vim":
+    file { "/home/${user}/.vim":
         ensure => "directory",
-        owner => "vagrant",
-        group => "vagrant",
+        owner => $user,
+        group => $group,
         alias => "vim-dir",
+        require => User["${user}"],
     }
     
-    file { "/home/vagrant/.vim/colors":
+    file { "/home/${user}/.vim/colors":
         ensure => "directory",
-        owner => "vagrant",
-        group => "vagrant",
+        owner => $user,
+        group => $group,
         alias => "vim-color-dir",
         require => File["vim-dir"],
     }
 
-    file { "/home/vagrant/.vim/colors/molokai.vim":
+    file { "/home/${user}/.vim/colors/molokai.vim":
         ensure => present,
-        owner => "vagrant",
-        group => "vagrant",
+        owner => $user,
+        group => $group,
         source => "puppet:///modules/vagrant_base/molokai.vim",
         require => File["vim-color-dir"],
     }
