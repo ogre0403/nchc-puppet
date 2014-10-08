0a3c56ab03ce19a4f3de12cf78d063da9099cabe
root <root@puppet-master>
2014-10-08 00:48:23 +0000

    add zookeeper module

---
+++ zookeeper/manifests/params.pp
@@ -0,0 +1,30 @@
+# /etc/puppet/modules/zookeeper/manafests/init.pp
+
+class zookeeper::params{
+
+    $zk_version = $::hostname ? {
+       default => "zookeeper-3.4.5-cdh4.7.0",
+    }
+
+    $zk_base = $::hostname ? {
+        default     => "/opt/zookeeper_version",
+    }
+
+    $zk_current = $::hostname ? {
+        default     => "/opt/zookeeper",
+    }
+ 
+    $zk_data_path = $::hostname ? {
+        default     => "/tmp/zookeeper/data",
+    }
+
+    $zk_log_path = $::hostname ? {
+        default     => "/tmp/zookeeper/log",
+    }
+
+    $zk_servers = $::hostname ? {
+        default         => ["NN-agent"] 
+    }
+
+    
+}
