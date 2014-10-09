9d254ee4b40003292866cdbbed71d75af1e3b9ec
root <root@puppet-master>
2014-10-09 09:20:14 +0000

    move vagrant_base module to submodule under nchc

---
+++ nchc/manifests/base/install.pp
@@ -0,0 +1,75 @@
+# /etc/puppet/modules/vagrant_base/manifests/init.pp
+
+class nchc::base::install(
+        $user = 'vagrant', 
+        $group = 'vagrant', 
+    ) {
+
+    user { "${user}":
+        ensure     => "present",
+        managehome => true,
+        shell  => "/bin/bash",
+    }
+
+ 
+    $vim = $operatingsystem ? {
+      centos                => 'vim-enhanced.x86_64',
+      redhat                => 'vim-enhanced.x86_64',
+      /(?i)(ubuntu|debian)/ => 'vim',
+      default               => undef,
+    }
+
+    $screen = $operatingsystem ? {
+      centos                => 'screen.x86_64',
+      redhat                => 'screen.x86_64',
+      /(?i)(ubuntu|debian)/ => 'screen',
+      default               => undef,
+    }
+
+	package { ["${vim}", "${screen}"]:
+		ensure => installed,
+	}
+
+    file { "/home/${user}/.screenrc":
+        ensure => present,
+        owner => $user,
+        group => $group,
+        alias => "screen-config",
+        source => "puppet:///modules/nchc/base/screenrc",
+        require => [Package["${screen}"], User["${user}"]],
+    }
+
+    file { "/home/${user}/.vimrc":
+        ensure => present,
+        owner => $user,
+        group => $group,
+        alias => "vim-config",
+        source => "puppet:///modules/nchc/base/vimrc",
+        require => [Package["${vim}"], User["${user}"]],
+    }
+
+
+    file { "/home/${user}/.vim":
+        ensure => "directory",
+        owner => $user,
+        group => $group,
+        alias => "vim-dir",
+        require => User["${user}"],
+    }
+    
+    file { "/home/${user}/.vim/colors":
+        ensure => "directory",
+        owner => $user,
+        group => $group,
+        alias => "vim-color-dir",
+        require => File["vim-dir"],
+    }
+
+    file { "/home/${user}/.vim/colors/molokai.vim":
+        ensure => present,
+        owner => $user,
+        group => $group,
+        source => "puppet:///modules/nchc/base/molokai.vim",
+        require => File["vim-color-dir"],
+    }
+}
