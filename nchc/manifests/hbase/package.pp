41713dfe612aac0fa4ef5a036c493eba86350273
vagrant <vagrant@puppet>
2014-10-12 07:03:49 +0000

    move nchc_hbase module to nchc::hbase sub module

--- nchc/manifests/hbase/package.pp
+++ nchc/manifests/hbase/package.pp
@@ -1,90 +1,65 @@
 # /etc/puppet/modules/zookeeper/manafests/init.pp
 
-class nchc_hbase {
+class nchc::hbase::package {
 
-    require nchc_hbase::params
-    require hadoop::params
-    require zookeeper::params
+    require nchc::params::hbase
+    #require hadoop::params
+    #require zookeeper::params
     
 
-    file {"$nchc_hbase::params::hbase_base":
+    file {"$nchc::params::hbase::hbase_base":
         ensure => "directory",
-        owner => "${hadoop::params::hdadm}",
-        group => "${hadoop::params::hdgrp}",
+        owner => "${nchc::params::hbase::hbase_adm}",
+        group => "${nchc::params::hbase::hbase_grp}",
         alias => "hbase-base",
         before => File["hbase-tgz"],
     }
 
 
-    file { "${nchc_hbase::params::hbase_base}/${nchc_hbase::params::hbase_version}.tar.gz":
+    file { "${nchc::params::hbase::hbase_base}/${nchc::params::hbase::hbase_version}.tar.gz":
         ensure => present,
-        owner => "${hadoop::params::hdadm}",
-        group => "${hadoop::params::hdgrp}",
+        owner => "${nchc::params::hbase::hbase_adm}",
+        group => "${nchc::params::hbase::hbase_grp}",
         alias => "hbase-tgz",
         before => Exec["untar-hbase"],
-        source => "puppet:///modules/nchc_hbase/tarball/${nchc_hbase::params::hbase_version}.tar.gz",
+        source => "puppet:///modules/nchc/hbase/tarball/${nchc::params::hbase::hbase_version}.tar.gz",
         require => File["hbase-base"],
     }
 
-    exec { "untar ${nchc_hbase::params::hbase_version}.tar.gz":
-        command => "tar xfvz ${nchc_hbase::params::hbase_version}.tar.gz",
-        cwd => "${nchc_hbase::params::hbase_base}",
-        creates => "${nchc_hbase::params::hbase_base}/${nchc_hbase::params::hbase_version}",
+    exec { "untar ${nchc::params::hbase::hbase_version}.tar.gz":
+        command => "tar xfvz ${nchc::params::hbase::hbase_version}.tar.gz",
+        cwd => "${nchc::params::hbase::hbase_base}",
+        creates => "${nchc::params::hbase::hbase_base}/${nchc::params::hbase::hbase_version}",
         alias => "untar-hbase",
         user => "root",
-        onlyif => "test 0 -eq $(ls -al ${nchc_hbase::params::hbase_base}/${nchc_hbase::params::hbase_version} | grep -c bin)",
+        onlyif => "test 0 -eq $(ls -al ${nchc::params::hbase::hbase_base}/${nchc::params::hbase::hbase_version} | grep -c bin)",
         before => File["hbase-app-dir"],
         path   => ["/bin", "/usr/bin", "/usr/sbin"],
         require => File["hbase-tgz"],
     }
 
-    file { "${nchc_hbase::params::hbase_base}/${nchc_hbase::params::hbase_version}":
+    file { "${nchc::params::hbase::hbase_base}/${nchc::params::hbase::hbase_version}":
         ensure => "directory",
-        owner => "${hadoop::params::hdadm}",
-        group => "${hadoop::params::hdgrp}",
+        owner => "${nchc::params::hbase::hbase_adm}",
+        group => "${nchc::params::hbase::hbase_grp}",
         alias => "hbase-app-dir",
         require => Exec["untar-hbase"],
     }
 
-    exec{ "chown ${nchc_hbase::params::hbase_version} dir":
-        command => "chown ${hadoop::params::hdadm}:${hadoop::params::hdadm} -R ${nchc_hbase::params::hbase_version}",
-        cwd => "${nchc_hbase::params::hbase_base}/",
+    exec{ "chown ${nchc::params::hbase::hbase_version} dir":
+        command => "chown ${nchc::params::hbase::hbase_adm}:${nchc::params::hbase::hbase_grp} -R ${nchc::params::hbase::hbase_version}",
+        cwd => "${nchc::params::hbase::hbase_base}/",
         path   => ["/bin", "/usr/bin", "/usr/sbin"],
         user => "root",
-        onlyif => "test ${hadoop::params::hdadm} != $(stat -c %U ${nchc_hbase::params::hbase_base}/${nchc_hbase::params::hbase_version}/bin)",
+        onlyif => "test ${nchc::params::hbase::hbase_adm} != $(stat -c %U ${nchc::params::hbase::hbase_base}/${nchc::params::hbase::hbase_version}/bin)",
         require => File["hbase-app-dir"],
     }
 
-    file { "$nchc_hbase::params::hbase_current":
+    file { "$nchc::params::hbase::hbase_current":
         ensure => 'link',
-        target => "${nchc_hbase::params::hbase_base}/${nchc_hbase::params::hbase_version}",
+        target => "${nchc::params::hbase::hbase_base}/${nchc::params::hbase::hbase_version}",
         alias => "hbase-link",
         require => File["hbase-app-dir"],
-    }
-
-    
-    file { "${nchc_hbase::params::hbase_base}/${nchc_hbase::params::hbase_version}/conf/regionservers":
-        owner => "${hadoop::params::hdadm}",
-        group => "${hadoop::params::hdgrp}",
-        alias => "RS",
-        require => File["hbase-app-dir"],
-        content => template("nchc_hbase/conf/regionservers.erb"),
-    }
-
-    file { "${nchc_hbase::params::hbase_base}/${nchc_hbase::params::hbase_version}/conf/hbase-env.sh":
-        owner => "${hadoop::params::hdadm}",
-        group => "${hadoop::params::hdgrp}",
-        alias => "hbase-env",
-        require => File["hbase-app-dir"],
-        content => template("nchc_hbase/conf/hbase-env.sh.erb"),
-    }
-
-    file { "${nchc_hbase::params::hbase_base}/${nchc_hbase::params::hbase_version}/conf/hbase-site.xml":
-        owner => "${hadoop::params::hdadm}",
-        group => "${hadoop::params::hdgrp}",
-        alias => "hbase-site",
-        require => File["hbase-app-dir"],
-        content => template("nchc_hbase/conf/hbase-site.xml.erb"),
     }
 
 }
--- nchc/manifests/params/hbase.pp
+++ nchc/manifests/params/hbase.pp
@@ -1,6 +1,27 @@
 # /etc/puppet/modules/nchc-hbase/manafests/params.pp
 
 class nchc::params::hbase {
+
+    $hbase_adm = $::hostname ? {
+       default => "vagrant",
+    }
+
+    $hbase_grp = $::hostname ? {
+       default => "vagrant",
+    }
+
+    $hbase_jdk = $::hostname ? {
+       default => "/opt/java",
+    }
+
+    $hbase_hdfs = $::hostname ? {
+       default => "NN",
+    }
+
+    $hbase_hdfsport = $::hostname ? {
+       default => "9000",
+    }
+
     $hbase_version = $::hostname ? {
        default => "hbase-0.94.15-cdh4.7.0",
     }
@@ -14,15 +35,15 @@ class nchc::params::hbase {
     }
 
     $hbase_master = $::hostname ? {
-        default     => ["NN-agent"]
+        default     => ["NN"]
     }
 
     $hbase_regionservers = $::hostname ? {
-        default     => ["DN1-agent"]
+        default     => ["DN1"]
     }
 
     $zookeeper_quorum = $::hostname ? {
-        default     => ["NN-agent"],
+        default     => ["NN"],
     }
  
     $hbase_dir = $::hostname ? {
